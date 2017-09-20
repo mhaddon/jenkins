ecc0ea845acba2828b0742df8311e5071a60862f
Michael Haddon <dis_connected@ymail.com>
2017-09-20 16:00:06 +0200

    sav

--- server.groovy
+++ server.groovy
@@ -1,15 +1,14 @@
 pipelineJob('Test') {
-    scm {
-        git('https://github.com/mhaddon/SpringBoot-AirportDemoProject.git', 'tests')
-    }
+//    scm {
+//        git('https://github.com/mhaddon/SpringBoot-AirportDemoProject.git', 'tests')
+//    }
+
     triggers {
         scm('H/15 * * * *')
     }
-    steps {
-        maven {
-            mavenInstallation('myMaven')
-            goals('clean')
-            goals('compile')
+    definition {
+        cps {
+            script(readFileFromWorkspace('Jenkinsfile'))
         }
     }
 }
