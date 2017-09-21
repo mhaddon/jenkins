017f8cab0908f7406812fc53f39baaff673564e1
Michael Haddon <dis_connected@ymail.com>
2017-09-21 11:05:44 +0200

    c

--- server.groovy
+++ server.groovy
@@ -1,16 +1,16 @@
 pipelineJob('Test') {
-    configure { project ->
-        project / builders << 'org.foo.FooBuilder' {
-            userName('a')
-            password('a')
-            credentialsId('ojfsa')
-        }
-    }
-
     definition{
         cpsScm {
             scm {
-                git('https://github.com/mhaddon/SpringBoot-AirportDemoProject.git', 'tests')
+                git('https://github.com/mhaddon/SpringBoot-AirportDemoProject.git', 'tests') {
+                    configure { project ->
+                        project / builders << 'org.foo.FooBuilder' {
+                            userName('a')
+                            password('a')
+                            credentialsId('ojfsa')
+                        }
+                    }
+                }
             }
         }
     }
